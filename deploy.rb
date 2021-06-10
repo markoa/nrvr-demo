b0dd25001ee196994da20830de4f3915d0151a70
Marko Anastasov <marko@renderedtext.com>
2021-06-10 12:51:46 +0200

    Use the secret key in deployment

--- deploy.rb
+++ deploy.rb
@@ -2,4 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'faker'
 
+deploy_key = ARGV[1]
+puts "Deploy key is in place" if deploy_key.size > 0
+
 puts "Deploying to #{ARGV[0]} at #{Faker::Internet.url}..."
